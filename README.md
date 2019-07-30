4133cff0c493dd3da3cde1f68c786e37dbf5d7e2
Thiam Hock Ng <nthock@gmail.com>
2019-07-31 00:18:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# IslandsEngine
+
+Code with tests and typespecs according to the book - Functional Web Development with Elixir, OTP and Phoenix
+
---
+++ test/islands_engine/guesses_test.exs
@@ -0,0 +1,15 @@
+defmodule IslandsEngine.GuessesTest do
+  use ExUnit.Case
+  alias IslandsEngine.Guesses
+
+  describe "new/2" do
+    # test "should return the coordinate struct when input the correct value" do
+    #   assert {:ok, %Coordinate{}} = Coordinate.new(1, 1)
+    # end
+
+    # test "should return error if the coordinate is not in range" do
+    #   assert {:error, :invalid_coordinate} = Coordinate.new(-1, 1)
+    #   assert {:error, :invalid_coordinate} = Coordinate.new(11, 1)
+    # end
+  end
+end
